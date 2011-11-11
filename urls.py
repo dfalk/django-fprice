941c02bb29949fcbebe241dc73a79471ce777f0c
dfalk <tfalc@mail.ru>
2011-11-11 16:48:24 +0300

    fix urls

--- urls.py
+++ urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('fprice.views',
 
     # List views
-    url(r'^$', view='price_list', name='price_index'),
+    #url(r'^$', view='price_list', name='price_index'),
+    url(r'^price/$', view='price_list', name='price_index'),
     url(r'^trade/all/$', view='trade_admin', name='price_trade_admin'),
     url(r'^trade/$', view='trade_list', name='price_trade_list'),
     url(r'^product/(?P<product_id>\d+)/$', view='product_detail', name='price_product_detail'),
