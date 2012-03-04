9b4ce54ebfb011f074763909fb19e8a4a4b31d5a
Dmitry Falk <tfalc@mail.ru>
2012-03-04 15:25:47 +0400

    setuptools layout

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-fprice',
+    version='0.1',
+    description='Price app for Django Web Framework',
+    author='PF',
+    author_email='dfalk@gmail.com',
+    packages=find_packages(),
+    include_package_data = True,
+    # TODO ? templates and static
+    # TODO install_requires=[''],
+)
+
