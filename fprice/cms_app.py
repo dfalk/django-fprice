c35a30fd39fd6f4f8c937be69cfca7ba3a223a57
Dmitry Falk <tfalc@mail.ru>
2012-03-09 01:12:56 +0400

    import menu

--- fprice/cms_app.py
+++ fprice/cms_app.py
@@ -1,6 +1,7 @@
 from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
 from django.utils.translation import ugettext_lazy as _
+from fprice.menu import PriceMenu
 
 class PriceApp(CMSApp):
     name = _("Price App") # give your app a name, this is required
